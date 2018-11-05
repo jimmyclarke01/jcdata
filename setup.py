d15aee660ea95aac45080a97181fdae624158c32
James <james.clarke42@gmail.com>
2018-11-05 11:26:34 +0000

    Fixed error in setup.py

--- setup.py
+++ setup.py
@@ -6,6 +6,6 @@ setup(
    description='A useful module',
    author='James Clarke',
    author_email='james.clarke42@gmail.com',
-   packages=['jcdatadata'],  #same as name
+   packages=['jcdata'],  #same as name
    install_requires=['pandas'], #external packages as dependencies
 )
